60bed555b5b91ae18b198ad0f4b958b67e02fe16
baronchibuikem <baronchibuike@gmail.com>
2020-07-18 17:12:17 +0100

    Added link to the serve side for API calls

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL:
 		process.env.NODE_ENV === "development"
 			? "http://127.0.0.1:8000"
-			: "https://healthdbbackend.herokuapp.com/"
+			: "https://pollsurvey.herokuapp.com/"
 });
 
 // export default axios.create({
