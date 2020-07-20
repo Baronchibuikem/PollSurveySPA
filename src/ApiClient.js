40f51373945d30e04d3bf5dd0ca325d5515c1ee4
baronchibuikem <baronchibuike@gmail.com>
2020-07-20 11:39:24 +0100

    fixed a bug

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export default axios.create({
-	baseURL:
-		process.env.NODE_ENV === "development"
-			? "http://127.0.0.1:8000/api/v1/"
-			: "http://pollsurvey.herokuapp.com/"
-});
-
 // export default axios.create({
-// 	baseURL: "http://pollsurvey.herokuapp.com/api/v1/"
+// 	baseURL:
+// 		process.env.NODE_ENV === "development"
+// 			? "http://127.0.0.1:8000/api/v1/"
+// 			: "http://pollsurvey.herokuapp.com/"
 // });
+
+export default axios.create({
+	baseURL: "http://pollsurvey.herokuapp.com/api/v1/"
+});
