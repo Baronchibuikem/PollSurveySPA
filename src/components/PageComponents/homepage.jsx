5c711112d57232f208f77cee3fb40b83dda936db
baronchibuikem <baronchibuike@gmail.com>
2020-08-11 22:06:28 +0100

    fixed login bug

--- src/components/PageComponents/homepage.jsx
+++ src/components/PageComponents/homepage.jsx
@@ -3,24 +3,24 @@ import AllPolls from "./allPolls";
 import CreatePoll from "./createPoll";
 import ProfileHeader from "./profileHeader";
 import GetTrends from "./getTrends"
-import ErrorBoundary from "antd/lib/alert/ErrorBoundary";
+// import ErrorBoundary from "antd/lib/alert/ErrorBoundary";
 
 export default class Homepage extends Component {
 	render() {
 		return (
 			<div className="row container mx-auto" style={{ paddingTop: "30px" }}>
 				<div className="col-md-3">
-					<ErrorBoundary>
-						<ProfileHeader />
-					</ErrorBoundary>
+					{/* <ErrorBoundary> */}
+					<ProfileHeader />
+					{/* </ErrorBoundary> */}
 				</div>
 				<div className="col-md-6">
-					<ErrorBoundary>
-						<CreatePoll />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<AllPolls />
-					</ErrorBoundary>
+					{/* <ErrorBoundary> */}
+					<CreatePoll />
+					{/* </ErrorBoundary> */}
+					{/* <ErrorBoundary> */}
+					<AllPolls />
+					{/* </ErrorBoundary> */}
 				</div>
 				<div className="col-md-3">
 					<GetTrends />
