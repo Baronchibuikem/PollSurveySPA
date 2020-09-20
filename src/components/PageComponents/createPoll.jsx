cf6948fe73c81d9fdc384bc97e2d99e01a77bc74
baronchibuikem <baronchibuike@gmail.com>
2020-09-20 08:20:20 +0100

    fixed a bug in creating poll action

--- src/components/PageComponents/createPoll.jsx
+++ src/components/PageComponents/createPoll.jsx
@@ -1,200 +1,190 @@
 import React, { useState } from "react";
 import { func, object } from "prop-types";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { defaultColor } from "../UtilityComponents/HelperFunctions";
 import { create_poll } from "../../store/actions/poll_action";
-import LoadSpinner from "../CommonComponents/Loadspinner"
-
+import LoadSpinner from "../CommonComponents/Loadspinner";
 
 const CreatePoll = () => {
-	const [question, setQuestion] = useState("");
-	const [option, setOption] = useState('')
-	const [options, setOptions] = useState([])
-	const [initialValue, setInitialValue] = useState([])
-	const [showForm, setShowForm] = useState(false)
-	const [date, setDate] = useState("")
+  const [question, setQuestion] = useState("");
+  const [option, setOption] = useState("");
+  const [options, setOptions] = useState([]);
+  const [initialValue, setInitialValue] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [date, setDate] = useState("");
 
-	const dispatch_createpoll = useDispatch()
+  const dispatch_createpoll = useDispatch();
 
-	const params = useSelector((state) => ({
-		token: state.userAuth.token,
-		error: state.userAuth.error,
-		status: state.userAuth.status
-	}));
+  const params = useSelector((state) => ({
+    token: state.userAuth.token,
+    error: state.userAuth.error,
+    status: state.userAuth.status,
+  }));
 
+  const focusActivated = (e) => {
+    setShowForm(true);
+    setQuestion(e.target.value);
+  };
 
-	const focusActivated = (e) => {
-		setShowForm(true)
-		setQuestion(e.target.value)
-	};
+  const focusDeactivated = () => {
+    setQuestion("");
+    setOptions([]);
+    setShowForm(false);
+  };
 
-	const focusDeactivated = () => {
-		setQuestion("")
-		setOptions([])
-		setShowForm(false)
-	};
+  // const addOptionToArray = () => {
+  // 	setInitialValue.choice_name.push({ choice_name: option })
+  // }
 
-	// const addOptionToArray = () => {
-	// 	setInitialValue.choice_name.push({ choice_name: option })
-	// }
+  const AddOption = () => {
+    // addOptionToArray()
+    // const list = setInitialValue.choice_name.concat({ choice_name: option })
+    // setInitialValue({ choice_name: list })
+    setOptions((options) => {
+      return [...options, option];
+    });
+    console.log(initialValue);
+    setOption("");
+  };
 
-	const AddOption = () => {
-		// addOptionToArray()
-		// const list = setInitialValue.choice_name.concat({ choice_name: option })
-		// setInitialValue({ choice_name: list })
-		setOptions(options => {
-			return [...options, option]
-		})
-		console.log(initialValue)
-		setOption("")
-	};
+  // For removing an a choice from a poll question when it is being created
+  const RemoveOption = (o) => {
+    const newArr = options.filter((opt) => opt !== o);
+    setOptions(newArr);
+    setOption("");
+  };
 
-	// For removing an a choice from a poll question when it is being created
-	const RemoveOption = o => {
-		const newArr = options.filter(opt => opt !== o);
-		setOptions(newArr);
-		setOption("")
+  const submit = (e) => {
+    e.preventDefault();
+    const token = params.token;
+    const choices =
+      options && options.length
+        ? options.map((option) => {
+            return {
+              choice_name: option,
+            };
+          })
+        : [];
 
-	};
+    // console.log(question, options, date, "FROM Poll SUBMIT", token)
+    dispatch_createpoll(create_poll({ question, choices, date, token }));
+    setQuestion(question);
+    setOption("");
+    setOptions([]);
+    setInitialValue([]);
+    setDate("");
+    setShowForm(false);
+  };
 
-	const submit = (e) => {
-		// e.preventDefault()
-		const token = params.token
-		const choices = options && options.length ? options.map(option => {
-			return {
-				choice_name: option
-			}
-		}) : []
+  const choiceform = (
+    <div className="d-flex">
+      <input
+        type="text"
+        placeholder="Enter Choices for this poll (Optional)"
+        className="form-control"
+        style={{ borderRadius: "5px" }}
+        value={option}
+        readOnly={!question}
+        onChange={(e) => setOption(e.target.value)}
+      />
 
-		// console.log(question, options, date, "FROM Poll SUBMIT", token)
-		dispatch_createpoll(create_poll({ question, choices, date, token }));
-		setQuestion("")
-		setOption("")
-		setOptions([])
-		setInitialValue([])
-		setDate("")
-		// setShowForm(false)
-	}
+      <button
+        type="button"
+        onClick={AddOption}
+        disabled={!option}
+        className="form-control w-25 b"
+        style={
+          option ? defaultColor.background_color : { backgroundColor: "grey" }
+        }
+      >
+        Add
+      </button>
+    </div>
+  );
+  return (
+    <div>
+      <form>
+        <div>
+          <textarea
+            style={{ borderRadius: "5px" }}
+            className="form-control is-rounded"
+            onChange={focusActivated}
+            placeholder="What is on your mind"
+            maxLength="150"
+          ></textarea>
+        </div>
+        <div className="options">
+          {options.map((option, index) => {
+            return (
+              <div key={index}>
+                <hr />
+                <h6 onClick={() => RemoveOption(option)} className=" mx-1">
+                  {option}
+                  <small
+                    className="text-danger mx-1"
+                    onClick={() => RemoveOption(option)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <i className="fa fa-trash"></i>
+                  </small>
+                </h6>
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+        {showForm && <div className="mt-3">{choiceform}</div>}
 
-	const choiceform = (
-		<div className="d-flex">
-			<input
-				type="text"
-				placeholder="Enter Choices for this poll (Optional)"
-				className="form-control"
-				style={{ borderRadius: "5px" }}
-				value={option}
-				readOnly={!question}
-				onChange={(e) => setOption(e.target.value)}
-			/>
+        {showForm && (
+          <div className="mt-3">
+            <input
+              type="date"
+              value={date}
+              className="form-control is-rounded"
+              style={{ borderRadius: "5px" }}
+              disabled={Boolean(!options.length)}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        )}
 
-			<button
-				type="button"
-				onClick={AddOption}
-				disabled={!option}
-				className="form-control w-25 b"
-				style={
-					option
-						? defaultColor.background_color
-						: { backgroundColor: "grey" }
-				}>
-				Add
-				</button>
-		</div>
-	);
-	return (
-		<div>
-			<form>
-				<div>
-					<textarea
-						style={{ borderRadius: "5px" }}
-						className="form-control is-rounded"
-						onChange={focusActivated}
-						placeholder="What is on your mind"
-						maxLength="150"></textarea>
-				</div>
-				<div className="options">
-					{options.map((option, index) => {
-						return (
-							<div key={index}>
-								<hr />
-								<h5 onClick={() => RemoveOption(option)} className=" mx-1">
-									{option}
-									<small
-										className="text-danger mx-1"
-										onClick={() => RemoveOption(option)}
-										style={{ cursor: "pointer" }}>
-										<i className="fa fa-trash"></i>
-									</small>
-								</h5><hr />
-
-							</div>
-						);
-					})}
-				</div>
-				{
-					showForm && (
-						<div className="mt-3">{choiceform}</div>
-					)
-				}
-
-				{
-					showForm && (
-						<div className="mt-3">
-							<input
-								type="date"
-								value={date}
-								className="form-control is-rounded"
-								style={{ borderRadius: "5px" }}
-								disabled={Boolean(!options.length)}
-								onChange={(e) => setDate(e.target.value)}
-							/>
-
-						</div>
-					)
-				}
-
-				{
-					showForm && (
-						<div className="d-flex">
-							<button
-								onClick={submit}
-								disabled={!question && !options && !date}
-								className="form-control mt-3"
-								style={
-									question
-										? defaultColor.background_color
-										: { backgroundColor: "grey" }
-								}>
-								{
-									params.status ?
-										<div>
-											<span>Loading...</span>
-										</div>
-										: "Submit"
-								}
-							</button>
-							<button
-								className="form-control mt-3"
-								style={defaultColor.background_color}
-								onClick={focusDeactivated}
-							>
-								Cancel
-							</button>
-						</div>
-					)
-				}
-
-			</form>
-
-		</div>
-	);
-}
+        {showForm && (
+          <div className="d-flex">
+            <button
+              onClick={submit}
+              disabled={!question && !options && !date}
+              className="form-control mt-3"
+              style={
+                question
+                  ? defaultColor.background_color
+                  : { backgroundColor: "grey" }
+              }
+            >
+              {params.status ? (
+                <div>
+                  <span>Loading...</span>
+                </div>
+              ) : (
+                "Submit"
+              )}
+            </button>
+            <button
+              className="form-control mt-3"
+              style={defaultColor.background_color}
+              onClick={focusDeactivated}
+            >
+              Cancel
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
 
 CreatePoll.propType = {
-	defaultColor: object,
-	Logger: func
-}
+  defaultColor: object,
+  Logger: func,
+};
 
 export default CreatePoll;
-
--- src/store/actions/poll_action.js
+++ src/store/actions/poll_action.js
@@ -1,145 +1,176 @@
 import {
-    ALLPOLL, ALLPOLL_FAIL, VOTE_FAIL, SINGLEPOLL, CREATEPOLL_FAIL,
-    REQUEST_ERROR, REQUEST_LOADING, SINGLEPOLL_FAIL
-} from "../actions/actionTypes"
+  ALLPOLL,
+  ALLPOLL_FAIL,
+  VOTE_FAIL,
+  SINGLEPOLL,
+  CREATEPOLL_FAIL,
+  REQUEST_ERROR,
+  REQUEST_LOADING,
+  SINGLEPOLL_FAIL,
+} from "../actions/actionTypes";
 import { route } from "../../ApiClient";
 import { callApi } from "../index";
-
 
 /* 
 This can also be used to send a post request to the backend, but i choose not to use it, since the
 imported callApi has been probably customized and made reusuable for making CRUD request.
-----------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------- */
 export const create_poll = (data) => {
-    let config = {
-        headers: {
-            Authorization: `Token ${data.token}`,
-            "Content-Type": "application/json"
+  let config = {
+    headers: {
+      Authorization: `Token ${data.token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  return async (dispatch) => {
+    try {
+      const response = await route.post(
+        "polls/create-polls/",
+        {
+          poll_question: data.question,
+          choices: data.choices,
+          poll_expiration_date: data.date,
         },
-    };
-
-    return async dispatch => {
-        const response = await route.post("polls/create-polls/", {
-            poll_question: data.question, choices: data.options, poll_expiration_date: data.date
-        }, config)
-        dispatch({ type: ALLPOLL, payload: response.data });
-    };
-    -------------------------------------------------------------------------------------
-}; */
-
-/* Used for sending data to the backend to create a new poll */
-export const create_poll = (data) => {
-    return async dispatch => {
-        try {
-            dispatch({ type: REQUEST_LOADING })
-            const response = await callApi("polls/create-polls/", {
-                poll_question: data.question,
-                choices: data.choices,
-                poll_expiration_date: data.date
-            }, "POST", data.token)
-            if (response.status === 200) {
-                dispatch(get_polls())
-                dispatch({ type: REQUEST_ERROR })
-            }
-        } catch (error) {
-            dispatch({ type: REQUEST_ERROR })
-            dispatch({
-                type: REQUEST_ERROR, payload: error && error.response ? error.response :
-                    "Couldn't create the poll"
-            })
-        }
-
-    };
+        config
+      );
+      if (response.status === 201) {
+        dispatch(get_polls());
+        dispatch({ type: REQUEST_ERROR });
+      }
+    } catch (error) {
+      dispatch({
+        type: REQUEST_ERROR,
+        payload:
+          error && error.response ? error.response : "Couldn't create the poll",
+      });
+    }
+  };
 };
 
-export const get_polls = () => {
-    return async (dispatch, getState) => {
-        const token = getState().userAuth.token
-        let config = {
-            headers: {
-                Authorization: `Token ${token}`,
-                "Content-Type": "application/json"
-            },
-        };
-        try {
-            const response = await route.get("polls/all-polls/",
-                config)
-            if (response) {
-                dispatch({ type: ALLPOLL, payload: response.data });
-            }
-        } catch (error) {
-            dispatch({ type: ALLPOLL_FAIL, payload: error.response })
-        }
+/* Used for sending data to the backend to create a new poll 
+export const create_poll = (data) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST_LOADING });
+      const response = await route.post(
+        "polls/create-polls/",
+        {
+          poll_question: data.question,
+          choices: data.choices,
+          poll_expiration_date: data.date,
+        },
+        data.token
+      );
+      if (response.status === 200) {
+        dispatch(get_polls());
+        dispatch({ type: REQUEST_ERROR });
+      }
+    } catch (error) {
+      dispatch({ type: REQUEST_ERROR });
+      dispatch({
+        type: REQUEST_ERROR,
+        payload:
+          error && error.response ? error.response : "Couldn't create the poll",
+      });
+    }
+  };
+};*/
 
+export const get_polls = () => {
+  return async (dispatch, getState) => {
+    const token = getState().userAuth.token;
+    let config = {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
     };
+    try {
+      const response = await route.get("polls/all-polls/", config);
+      if (response) {
+        dispatch({ type: ALLPOLL, payload: response.data });
+      }
+    } catch (error) {
+      dispatch({ type: ALLPOLL_FAIL, payload: error.response });
+    }
+  };
 };
 
 export const get_single_poll = (data) => {
-    return async (dispatch, getState) => {
-        const token = getState().userAuth.token
-        let config = {
-            headers: {
-                Authorization: `Token ${token}`,
-                "Content-Type": "application/json"
-            },
-        };
-        try {
-            const response = await route.get(`polls/all-polls/${data}/`,
-                config)
-            if (response) {
-                dispatch({ type: SINGLEPOLL, payload: response.data });
-            }
-        } catch (error) {
-            dispatch({ type: SINGLEPOLL_FAIL, payload: error.response })
-        }
-
+  return async (dispatch, getState) => {
+    const token = getState().userAuth.token;
+    let config = {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
     };
+    try {
+      const response = await route.get(`polls/all-polls/${data}/`, config);
+      if (response) {
+        dispatch({ type: SINGLEPOLL, payload: response.data });
+      }
+    } catch (error) {
+      dispatch({ type: SINGLEPOLL_FAIL, payload: error.response });
+    }
+  };
 };
 
 export const post_currentuser_vote = (data) => {
-    const { poll_id, choice_id } = data
-    console.log(poll_id, choice_id, "CHOICE ID AND POLL ID")
-    return async (dispatch, getState) => {
-        const token = getState().userAuth.token
-        let config = {
-            headers: {
-                Authorization: `Token ${token}`,
-                "Content-Type": "application/json"
-            },
-        };
-        try {
-            const response = await route.post(`/polls/vote/${poll_id}/${choice_id}/`, {
-                choice_id: choice_id, poll_id: poll_id
-            },
-                config)
-            if (response) {
-                dispatch(get_polls())
-            }
-        } catch (error) {
-            dispatch({ type: VOTE_FAIL, payload: error.response.data.non_field_errors })
-        }
-
+  const { poll_id, choice_id } = data;
+  return async (dispatch, getState) => {
+    const token = getState().userAuth.token;
+    let config = {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
     };
+    try {
+      const response = await route.post(
+        `/polls/vote/${poll_id}/${choice_id}/`,
+        {
+          choice_id: choice_id,
+          poll_id: poll_id,
+        },
+        config
+      );
+      if (response) {
+        dispatch(get_polls());
+      }
+    } catch (error) {
+      dispatch({
+        type: VOTE_FAIL,
+        payload: error.response.data.non_field_errors,
+      });
+    }
+  };
 };
 
 export const post_delete_poll = (data) => {
-    console.log(data, "from poll patch")
-    return async (dispatch, getState) => {
-        const token = getState().userAuth.token
-        let config = {
-            headers: {
-                Authorization: `Token ${token}`,
-                "Content-Type": "application/json"
-            },
-        };
-        try {
-            const response = await route.delete(`/polls/all-polls/${data.poll_id}/`, config)
-            if (response) {
-                dispatch(get_polls())
-            }
-        } catch (error) {
-            dispatch({ type: VOTE_FAIL, payload: error.response.data.non_field_errors })
-        }
-
+  console.log(data, "from poll patch");
+  return async (dispatch, getState) => {
+    const token = getState().userAuth.token;
+    let config = {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
     };
+    try {
+      const response = await route.delete(
+        `/polls/all-polls/${data.poll_id}/`,
+        config
+      );
+      if (response) {
+        dispatch(get_polls());
+      }
+    } catch (error) {
+      dispatch({
+        type: VOTE_FAIL,
+        payload: error.response.data.non_field_errors,
+      });
+    }
+  };
 };
