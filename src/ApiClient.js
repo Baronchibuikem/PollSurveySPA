16fff550a41fc826995bf6b5e39b4aeb0cda3dd6
Baron Uche Chibuikem <38393563+Baronchibuikem@users.noreply.github.com>
2021-01-20 18:10:06 +0100

    Update ApiClient.js

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const route = axios.create({
-  baseURL : "http://pollsurvey.herokuapp.com/api/v1",
+  baseURL : "https://pollsurvey.herokuapp.com/api/v1",
 });
 
 // export const route = axios.create({ baseURL: "http://127.0.0.1:8000/api/v1" })
